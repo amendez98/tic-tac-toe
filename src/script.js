a0d306bf536d82d154648b664fd0f8953c751134
alexmendez <alexmenbar98@gmail.com>
2022-08-05 23:34:20 -0400

    Adding x and o logic

--- src/script.js
+++ src/script.js
@@ -1,8 +1,38 @@
-window.onload = function newGame() {
-    const game = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
-    console.log('test');
+function newGame() {
+    const game = ['', '', '', '', '', '', '', '', ''];
     for(let x=0; x<game.length; x++){
         document.getElementById(x).innerHTML = game[x];
-        console.log(game[x]);
     }
-};
+}
+
+function handleEvent(id){
+    addValue(id, currentPlayer);
+    changePlayerTurn();
+}
+
+function changePlayerTurn(){
+    if(currentPlayer === 'X'){
+        currentPlayer = 'O';
+    }
+    else if(currentPlayer === 'O'){
+        currentPlayer = 'X';
+    }
+    else{
+        console.log('error!');
+    }
+
+}
+
+function addValue(id, currentPlayer) {
+    document.getElementById(id).innerHTML = currentPlayer;
+}
+
+
+
+newGame();
+
+let currentPlayer = 'X';
+
+for(let x=0; x<9; x++){
+    document.getElementById(x).addEventListener('click', function() {handleEvent(x)});
+}
